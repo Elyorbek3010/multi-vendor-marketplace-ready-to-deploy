a3790227a6f1551e23d074b30ee1196460f4a0d5
Elyorbek <2311194e@jdu.uz>
2026-08-27 19:05:11 +0500

    Remove Celery from deploy scripts

--- backend/start.sh
+++ backend/start.sh
@@ -3,8 +3,5 @@ set -o errexit
 
 cd backend
 
-# Start Celery worker in the background
-celery -A config worker -l info --concurrency 1 &
-
 # Start Gunicorn server in the foreground
 gunicorn config.wsgi:application --bind 0.0.0.0:$PORT --workers 2
